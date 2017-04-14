f59c7d665af12cb37d7082fa6dc4c42dfd1111fd
Alexander Rosenberg Johansen <ajohansen@salesforce.com>
2017-04-13 17:21:10 -0700

    Update writer.py

--- dashboard/writer.py
+++ dashboard/writer.py
@@ -1,6 +1,5 @@
 import os
 import ujson as json
-import pyrebase
 import tabulate
 
 
@@ -41,36 +40,9 @@ class FileWriter(Writer):
             f.flush()
 
 
-class FirebaseWriter(Writer):
-
-    def __init__(self, name, delete_existing=False):
-        assert name, '"name" cannot be empty!'
-        fconfig = os.path.join(os.environ['HOME'], '.fb.config')
-        assert os.path.isfile(fconfig), 'Failed to load config file from {}'.format(fconfig)
-        with open(fconfig) as f:
-            config = json.load(f)
-
-        self.name = name
-        self.fb = pyrebase.initialize_app(config)
-        self.user = self.fb.auth().sign_in_with_email_and_password(config['email'], config['password'])
-        if delete_existing:
-            self.delete()
-
-    def delete(self):
-        self.user = self.fb.auth().refresh(self.user['refreshToken'])
-        self.fb.database().child('experiments').child(self.name).remove(self.user['idToken'])
-
-    def add(self, metrics):
-        self.user = self.fb.auth().refresh(self.user['refreshToken'])
-        try:
-            self.fb.database().child('experiments').child(self.name).push(metrics, self.user['idToken'])
-        except Exception as e:
-            print('Could not write to Dashboard')
-            print(e)
-
 
 if __name__ == '__main__':
-    w = FirebaseWriter('my_exp')
+    w = FileWriter('my_exp')
     import time
     for i in range(10):
         print(i)
